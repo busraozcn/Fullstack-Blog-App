f3ccb7c0d84a9c671b994c728677af12cef8bda9
Büşra <busrozcn0@gmail.com>
2024-08-21 11:01:51 +0300

    Update

--- Backend/myproject/myproject/settings.py
+++ Backend/myproject/myproject/settings.py
@@ -86,14 +86,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'newDatabase',
-        'USER': 'postgres',
-        'PASSWORD': 'cennet70',
-        'HOST': 'localhost',
-        'PORT': '5432',       
-    }
+#your database
 }
 
 REST_FRAMEWORK = {
